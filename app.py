e4d63f3105ba1de174906aceabde30a3ac2e4ff7
wako104 <willwakeford02@outlook.com>
2024-07-03 16:58:20 +0100

    Workspace combobox started

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ icon_path = os.path.join(script_dir, 'images', 'osam-logo.png')
 default_records_path = os.path.join(script_dir, 'config', 'default_records.json')
 
 class App(tk.Tk):
+    """
+    GUI Application for managing Cloudflare zones and DNS records.
+
+    This class initializes the app, which includes a sidebar, header and main content area.
+    """
     def __init__(self):
         super().__init__()
         self.title('OSAM Websites Cloudflare Tool')
@@ -28,7 +33,10 @@ class App(tk.Tk):
         self.sidebar = tk.Frame(self)
         self.sidebar.pack(side='left', fill='y')
         # dashboard
-        self.dashboard_btn = ttk.Button(self.sidebar, image=self.logo, command=lambda: self.switch_frame(Dashboard, 'Dashboard'), state='disabled')
+        self.dashboard_btn = ttk.Button(
+            self.sidebar, image=self.logo, 
+            command=lambda: self.switch_frame(Dashboard, 'Dashboard'), 
+            state='disabled')
         self.dashboard_btn.pack(padx=10, pady=13)
         # separator
         self.separator = ttk.Separator(self.sidebar)
@@ -36,11 +44,21 @@ class App(tk.Tk):
         # zones
         self.zones_label = ttk.Label(self.sidebar, text='Zones')
         self.zones_label.pack(padx=10, pady=5)
-        self.quick_add_zone_btn = ttk.Button(self.sidebar, text='Quick Add Zone', command=lambda: self.switch_frame(QuickAddZonePage, 'Quick Add Zone'), state='disabled')
+        self.quick_add_zone_btn = ttk.Button(
+            self.sidebar, text='Quick Add Zone', 
+            command=lambda: self.switch_frame(QuickAddZonePage, 'Quick Add Zone'), 
+            state='disabled')
         self.quick_add_zone_btn.pack(padx=10, pady=5)
-        self.add_zone_btn = ttk.Button(self.sidebar, text='Add Zone', command=lambda: self.switch_frame(AddZonePage, 'Add Zone'), state='disabled')
+        self.add_zone_btn = ttk.Button(
+            self.sidebar, text='Add Zone', 
+            command=lambda: self.switch_frame(AddZonePage, 'Add Zone'), 
+            state='disabled')
         self.add_zone_btn.pack(padx=10, pady=5)
-        self.rmv_zone_btn = ttk.Button(self.sidebar, text='Remove Zone', command=lambda: self.switch_frame(RemoveZonePage, 'Remove Zone'), state='disabled')
+        self.rmv_zone_btn = ttk.Button(
+            self.sidebar, 
+            text='Remove Zone', 
+            command=lambda: self.switch_frame(RemoveZonePage, 'Remove Zone'), 
+            state='disabled')
         self.rmv_zone_btn.pack(padx=10, pady=5)
         # separator
         self.separator = ttk.Separator(self.sidebar)
@@ -48,7 +66,10 @@ class App(tk.Tk):
         # records
         self.records_label = ttk.Label(self.sidebar, text='DNS Records')
         self.records_label.pack(padx=10, pady=5)
-        self.s_and_r_btn = ttk.Button(self.sidebar, text='Search and Replace', command=lambda: self.switch_frame(SearchAndReplacePage, 'DNS Search and Replace'), state='disabled')
+        self.s_and_r_btn = ttk.Button(
+            self.sidebar, text='Search and Replace', 
+            command=lambda: self.switch_frame(SearchAndReplacePage, 'DNS Search and Replace'), 
+            state='disabled')
         self.s_and_r_btn.pack(padx=10, pady=5)
 
         #------------- RIGHT SIDE -------------
@@ -64,7 +85,11 @@ class App(tk.Tk):
         self.header_label.pack(fill='both', padx=10, pady=10)
 
         #---------------- MAIN -----------------
-        self.main_placeholder = tk.Frame(self.right_side, highlightthickness=1, highlightbackground='black', bd=0)
+        self.main_placeholder = tk.Frame(
+            self.right_side, 
+            highlightthickness=1, 
+            highlightbackground='black', 
+            bd=0)
         self.main_frame = tk.Frame(self.main_placeholder)
 
         self.main_placeholder.pack(expand=True, fill='both', padx=(0,5), pady=(0,5))
@@ -74,6 +99,7 @@ class App(tk.Tk):
         self.switch_frame(ConnectionPage, 'Connect to Cloudflare Account')
 
     def switch_frame(self, frame_class, header_text, **kwargs):
+        """Switches frame of the main area"""
         if self.current_frame is not None:
             self.current_frame.pack_forget()
             self.current_frame.destroy()
@@ -85,6 +111,7 @@ class App(tk.Tk):
         self.header_label.config(text=header_text)
     
     def enable_sidebar(self):
+        """Enables sidebar options after user establishes connection"""
         self.dashboard_btn.config(state='normal')
         self.quick_add_zone_btn.config(state='normal')
         # self.add_zone_btn.config(state='normal')
@@ -93,11 +120,19 @@ class App(tk.Tk):
 
 
 class ConnectionPage(tk.Frame):
+    """
+    Allows the user to establish a connection to a Cloudflare account, either using environment 
+    variables or entering their email and api key. A connection must be established before the apps 
+    functionality is used.
+    """
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
 
-        env_conn_btn = ttk.Button(self, text='Connect Using Environment Variables', command=self.conn_with_env)
+        env_conn_btn = ttk.Button(
+            self, 
+            text='Connect Using Environment Variables', 
+            command=self.conn_with_env)
         env_conn_btn.pack(pady=5)
 
         separator = ttk.Separator(self, orient='horizontal')
@@ -115,6 +150,7 @@ class ConnectionPage(tk.Frame):
         conn_btn.pack(pady=5)
 
     def conn(self):
+        """Connect to Cloudflare account using input fields"""
         api_key = self.api_key.get()
         email = self.email.get()
 
@@ -125,6 +161,7 @@ class ConnectionPage(tk.Frame):
             messagebox.showerror('Error', 'Connection Failed')
 
     def conn_with_env(self):
+        """Connect to Cloudflare account using environment variables"""
         try:
             api_key = os.environ['CLOUDFLARE_API_KEY']
             email = os.environ['CLOUDFLARE_EMAIL']
@@ -138,6 +175,7 @@ class ConnectionPage(tk.Frame):
 
 
 class AccountSelect(tk.Frame):
+    """Tkinter Frame subclass """
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
@@ -156,7 +194,9 @@ class AccountSelect(tk.Frame):
 
     def select_account(self):
         global account_id
-        account_id = next((id for id, name in self.accounts if name == self.account_var.get()), None)
+        account_id = next(
+            (id for id, name in self.accounts if name == self.account_var.get()), 
+            None)
         if account_id:
             self.controller.enable_sidebar()
             self.controller.switch_frame(Dashboard, 'Dashboard')
@@ -184,22 +224,32 @@ class QuickAddZonePage(tk.Frame):
         with open(default_records_path, 'r') as file:
             self.default_records = json.load(file)
 
-        server_label = ttk.Label(self, text='Choose Server:')
-        server_label.pack(pady=10)
-
-        default_server = list(self.default_records.keys())[0] if self.default_records else ''
-        self.selected_server = tk.StringVar(value=default_server)
+        ttk.Label(self, text='Choose Server:').pack(pady=10)
+        self.default_server = list(self.default_records.keys())[0] if self.default_records else ''
+        self.selected_server = tk.StringVar(value=self.default_server)
         for option in self.default_records:
             ttk.Radiobutton(self, text=option, variable=self.selected_server, value=option).pack()
-
-        zone_entry_label = ttk.Label(self, text='Enter Domain Name: ')
-        zone_entry_label.pack(pady=(20,10))
+        
+        ttk.Label(self, text='Choose workspace:').pack(pady=10)
+        self.workspace = tk.StringVar(value='None')
+        self.workspace_options = ['None', 'Google', 'Microsoft 365']
+        self.workspace_dropdown = ttk.Combobox(
+            self, 
+            textvariable=self.workspace, 
+            values=self.workspace_options, 
+            state='readonly')
+        self.workspace_dropdown.pack()
+        
+        ttk.Label(self, text='Enter Domain Name: ').pack(pady=(20,10))
         
         self.zone_name_entry = ttk.Entry(self, font=('Times', 12), width=30)
         self.zone_name_entry.pack(pady=10)
 
-        add_zone_btn = ttk.Button(self, text='Create Domain with Default DNS Records', command=self.add_zone)
-        add_zone_btn.pack(pady=10)
+        self.add_zone_btn = ttk.Button(
+            self, 
+            text='Create Domain with Default DNS Records', 
+            command=self.add_zone)
+        self.add_zone_btn.pack(pady=10)
 
     def add_zone(self):
         self.zone_name = self.zone_name_entry.get().strip()
@@ -211,7 +261,9 @@ class QuickAddZonePage(tk.Frame):
             loading_dialog = LoadingDialog(self)
             try:
                 loading_dialog.update('Creating Zone...')
-                self.zone_id, self.name_servers = func.handle_zone_creation(self.zone_name, account_id)
+                self.zone_id, self.name_servers = func.handle_zone_creation(
+                    self.zone_name, 
+                    account_id)
                 loading_dialog.update('Setting SSL...')
                 func.handle_set_ssl(self.zone_id)
                 loading_dialog.update('Enabling Always Use HTTPS...')
@@ -219,7 +271,13 @@ class QuickAddZonePage(tk.Frame):
                 loading_dialog.update('Adding DNS Records...')
                 self.records = func.handle_add_dns_records(self.zone_id, self.selected_records)
                 loading_dialog.complete()
-                self.controller.switch_frame(ZoneCompletePage, f'Zone Created ({self.zone_name})', records=self.records, zone_name=self.zone_name, zone_id=self.zone_id, name_servers=self.name_servers)
+                self.controller.switch_frame(
+                    ZoneCompletePage, 
+                    f'Zone Created ({self.zone_name})', 
+                    records=self.records, 
+                    zone_name=self.zone_name, 
+                    zone_id=self.zone_id, 
+                    name_servers=self.name_servers)
             except Exception as e:
                 messagebox.showerror('Error', str(e))
                 loading_dialog.destroy()
@@ -252,7 +310,10 @@ class ZoneCompletePage(tk.Frame):
             ns_frame.pack(pady=5)
             ns_label = ttk.Label(ns_frame, text=f'Nameserver {i+1}: {name_server}')
             ns_label.pack(side='left')
-            ns_copy_btn = ttk.Button(ns_frame, text='📋', width=5, command=lambda ns=name_server: self.copy_to_clipboard(ns))
+            ns_copy_btn = ttk.Button(
+                ns_frame, text='📋', 
+                width=5, 
+                command=lambda ns=name_server: self.copy_to_clipboard(ns))
             ns_copy_btn.pack(side='right')
 
         records_label = ttk.Label(self, text=f'{len(self.records)} Records Added:')
@@ -315,7 +376,14 @@ class SearchAndReplacePage(tk.Frame):
             # search and replace dns in one domain
             # search and replace dns across all domains
 
-        self.record = RecordEntryFrame(self).pack(pady=10, fill='x', expand=True)
+        self.record = RecordEntryFrame(self)
+        self.record.pack(pady=10, fill='x', expand=True)
+
+        ttk.Button(self, text='+', command=self.get_record_data).pack()
+
+    def get_record_data(self):
+        data = self.record.get_json()
+        print(json.dumps(data, indent=2))
 
 
 class RecordEntryFrame(tk.Frame):
@@ -326,7 +394,11 @@ class RecordEntryFrame(tk.Frame):
         self.record_types = ['A', 'CNAME', 'MX', 'TXT']
 
         ttk.Label(self, text='Select Record Type:').pack(pady=5)
-        self.record_type_menu = ttk.Combobox(self, textvariable=self.record_type_var, values=self.record_types, state='readonly')
+        self.record_type_menu = ttk.Combobox(
+            self, 
+            textvariable=self.record_type_var, 
+            values=self.record_types, 
+            state='readonly')
         self.record_type_menu.pack(pady=5)
         self.record_type_menu.bind('<<ComboboxSelected>>', self.display_input_fields)
 
@@ -350,7 +422,10 @@ class RecordEntryFrame(tk.Frame):
 
         for i, field in enumerate(fields.get(record_type, [])):
             if i > 0:
-                ttk.Separator(self.input_frame, orient='vertical').pack(side='left', fill='y', padx=8)
+                ttk.Separator(self.input_frame, orient='vertical').pack(
+                    side='left', 
+                    fill='y', 
+                    padx=8)
             if field == 'Proxy Status':
                 self.create_proxy_switch()
             else:
@@ -365,8 +440,11 @@ class RecordEntryFrame(tk.Frame):
         proxy_label = ttk.Label(self.input_frame, text='Proxy Status:')
         proxy_label.pack(side='left')
 
-        self.proxy_var = tk.BooleanVar()
-        proxy_switch = ttk.Checkbutton(self.input_frame, variable=self.proxy_var, command=self.update_proxy_text)
+        self.proxy_var = tk.BooleanVar(value=True)
+        proxy_switch = ttk.Checkbutton(
+            self.input_frame, 
+            variable=self.proxy_var, 
+            command=self.update_proxy_text)
         proxy_switch.pack(side='left')
 
         self.proxy_status_text = tk.StringVar(value='On')
@@ -378,6 +456,27 @@ class RecordEntryFrame(tk.Frame):
             self.proxy_status_text.set('On')
         else:
             self.proxy_status_text.set('Off')
+
+    def get_json(self):
+        record_type = self.record_type_var.get()
+        data = {'type': record_type}
+
+        field_map = {
+            'Name': 'name',
+            'IP Address': 'content',
+            'Target': 'content',
+            'Mail Server': 'content',
+            'Content': 'content',
+            'Proxy Status': 'proxied',
+            'Priority': 'priority'
+        }
+
+        for field, entry in self.input_fields.items():
+            if field == 'Proxy Status':
+                data[field_map[field]] = entry.variable.get()
+            else:
+                data[field_map[field]] = entry.get()
+        return data
 
 
 class LoadingDialog(tk.Toplevel):
